7df283275a2b1f89d70f549af035abe3e45f4665
Lloydmasteremerald <110808538+Lloydmasteremerald@users.noreply.github.com>
2023-05-01 21:33:05 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+  
+}
+
+function draw()
+{
+
+}
+
+function setup()
+{
+    Canvas = createCanvas(350, 350)   
+    Canvas.center();
+}
+
+function take_snapshot()
+{
+     save("Jay.png");
+}
